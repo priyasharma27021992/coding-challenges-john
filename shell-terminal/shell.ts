832a10af39ba0c93d452dc30f04c4cdb7ba7b462
Priya.Sharma <priyasharma2702@gmail.com>
2024-05-05 22:32:03 +0530

    added shell ts file

---
+++ shell-terminal/shell.ts
@@ -0,0 +1,35 @@
+import * as readline from 'readline';
+import { stdin, stdout } from 'process';
+import { ChildProcessWithoutNullStreams } from 'child_process';
+
+// // Using the readline package to get input from the user
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+function processCommand(input: string): ChildProcessWithoutNullStreams | null {
+  const inputArr = input.split(' ');
+
+  // the first word is command
+  const command = inputArr[0];
+
+  // The rest of the data is args to the command
+  const args = inputArr.splice(1, inputArr.length);
+}
+
+function handleInput(input: string) {
+  childProcess = processCommand(input);
+}
+
+/**
+ * Prompts user with ccsh> as prefix
+ * After the user provides the input (ENTER key is pressed)
+ * it calls {@link handleInput} function.
+ */
+function promptUser() {
+  rl.question('ccsh>', (input) => {
+    const cleanedInput = input.trim();
+    handleInput(cleanedInput);
+  });
+}
+
+// Entry point of the program
+promptUser();
