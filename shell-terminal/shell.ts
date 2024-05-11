5f8a7dbf9a3444701a8b89260943321bb47a9c41
Priya.Sharma <priyasharma2702@gmail.com>
2024-05-11 16:15:40 +0530

    completed shell terminal

--- shell-terminal/shell.ts
+++ shell-terminal/shell.ts
@@ -94,8 +94,104 @@ function processCommand(input: string): ChildProcessWithoutNullStreams | null {
   }
 }
 
+/**
+ * This function processes the given piped commands
+ * It creates multiple processes and pipes the output of the first process
+ * to the input of the subsequent process
+ * @param {string} input
+ * @returns {ChildProcessWithoutNullStreams | null}
+ */
+
+function processPipedCommand(
+  input: string,
+): ChildProcessWithoutNullStreams | null {
+  // Break the input into commands
+  const commands = input.split('|');
+
+  // This array will store all the processes
+  const processes = new Array<ChildProcessWithoutNullStreams>();
+
+  try {
+    // Initialize the first process
+    const input0Arr = commands[0].trim().split(' ');
+    const command0 = input0Arr[0];
+    const args0 = input0Arr.slice(1, input0Arr.length);
+    processes.push(spawn(command0, args0));
+
+    for (let i = 1; i < commands.length; i++) {
+      // Create a new processes based on the command
+      const inputI = commands[i].trim().split(' ');
+
+      // If no command is provided between 2 pipes
+      if (inputI.length === 0) {
+        stderr.write('No such file or diretory(os error 2)\n');
+        return null;
+      }
+
+      const commandI = inputI[0];
+      const argsI = inputI.slice(1, inputI.length);
+
+      const pi = spawn(commandI, argsI);
+
+      // Pipe the output of the previous process
+      processes[i - 1].stdout.pipe(pi.stdin);
+
+      // Push the new process into the array
+      processes.push(pi);
+
+      // Handle errors in the processes except the last one
+      processes[i - 1].on('error', () => {
+        stderr.write('No such file or directory(os error 2)\n');
+      });
+
+      processes[i - 1].stderr.on('data', (data) => {
+        stderr.write(data.toString());
+      });
+    }
+
+    // Return the last one
+    return processes[processes.length - 1];
+  } catch (err) {
+    stderr.write('No such file or directory(os error 2)\n');
+  }
+
+  return null;
+}
+
+/**
+ * Checks if the given command from the user in a piped command or not
+ *
+ * @param {string} input
+ * @returns {boolean}
+ */
+function checkIfPipeCommand(input: string): boolean {
+  return input.indexOf('|') > 0;
+}
+
+// Handle when the user sends CTRL + C
+process.on('SIGINT', () => {
+  // If a child process is connected then kill it
+  if (childProcess && childProcess.connected) {
+    childProcess.kill('SIGINT');
+    return;
+  }
+
+  // Otherwise write the history back to disk and exit
+  writeHistory();
+  process.exit(0);
+});
+
+/*
+ * This function takes in the input command and creates a child process
+ *  It attaches listeners to the stdout and stderr of the child process
+ * @param {string} input
+ */
 function handleInput(input: string) {
-  childProcess = processCommand(input);
+  if (checkIfPipeCommand(input)) {
+    childProcess = processPipedCommand(input);
+  } else {
+    childProcess = processCommand(input);
+  }
 
   if (childProcess != null) {
     childProcess.on('close', (code, signal) => {
